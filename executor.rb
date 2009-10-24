ad4faf788e78e7e22c68aa896dfa136c44f6a8c7
James Coleman <jtc331@gmail.com>
2009-10-24 15:44:48 -0400

    Added explanatory commenting to all of the conditional jump instructions. Added the JNO, JO, JNS, JS, JNAE, JNA, JNBE, JNP, JPE, JL, JGE, JLE, JG conditional jump instructions.

--- executor.rb
+++ executor.rb
@@ -25,6 +25,10 @@ module Executor
 		@state = :HALT_STATE
 	end
 	
+	# -----------------------------------------------------------------
+	# Arithemetic Instructions
+	# -----------------------------------------------------------------
+	
 	def execute_ADD(destination, source)
 		perform_arithmetic_operation(destination, destination.value + source.value)
 	end
@@ -49,12 +53,108 @@ module Executor
 		perform_arithmetic_operation(operand, operand.value - 1)
 	end
 	
-	def execute_JNE(operand)
+	# -----------------------------------------------------------------
+	# Control Transfer Instructions
+	# -----------------------------------------------------------------
+	
+	# Jump if the zero flag is set
+	# (from a math operation or comparing two numbers that are equal)
+	def execute_JNE(operand) # Same as the JNZ instruction
 		jump_conditionally_to_signed_displacement(operand, !(@flags.value[ZERO_FLAG].zero?)) # Zero means not set
 	end
 	
-	def execute_JE(operand)
+	# Jump if the zero flag is not set
+	# (from a math operation or comparing two numbers that are unequal)
+	def execute_JE(operand) # Same as the JZ instruction
 		jump_conditionally_to_signed_displacement(operand, @flags.value[ZERO_FLAG].zero?) # Non-zero means set
+	end
+	
+	# Jump if the overflow flag is not set
+	def execute_JNO(operand)
+		jump_conditionally_to_signed_displacement(operand, !(@flags.value[OVERFLOW_FLAG].zero?)) # Zero means not set
+	end
+	
+	# Jump if the overflow flag is set
+	def execute_JO(operand)
+		jump_conditionally_to_signed_displacement(operand, @flags.value[OVERFLOW_FLAG].zero?) # Non-zero means set
+	end
+	
+	# Jump if the sign flag is not set
+	def execute_JNS(operand)
+		jump_conditionally_to_signed_displacement(operand, !(@flags.value[SIGN_FLAG].zero?)) # Zero means not set
+	end
+	
+	# Jump if the sign flag is set
+	def execute_JS(operand)
+		jump_conditionally_to_signed_displacement(operand, @flags.value[SIGN_FLAG].zero?) # Non-zero means set
+	end
+	
+	# Jump if the carry flag is set
+	# (compare operation: either below || not above or equal)
+	def execute_JNAE(operand) # Same as the JC and JB instructions
+		jump_conditionally_to_signed_displacement(operand, !(@flags.value[CARRY_FLAG].zero?)) # Zero means not set
+	end
+	
+	# Jump if the carry flag is not set
+	# (compare operation: either not below || above or equal)
+	def execute_JNB(operand) # Same as the JNC and JAE instructions
+		jump_conditionally_to_signed_displacement(operand, @flags.value[CARRY_FLAG].zero?) # Non-zero means set
+	end
+	
+	# Jump if the carry and zero flags are not set
+	# (compare operation: either below or equal || not above)
+	def execute_JNA(operand) # Same as the JBE instruction
+		is_above = @flags.value[ZERO_FLAG].zero? && @flags.value[CARRY_FLAG].zero?
+		jump_conditionally_to_signed_displacement(operand, !is_above)
+	end
+	
+	# Jump if the carry and zero flags are not both set
+	# (compare operation: either not below or equal || above)
+	def execute_JNBE(operand) # Same as the JA instruction
+		is_above = @flags.value[ZERO_FLAG].zero? && @flags.value[CARRY_FLAG].zero?
+		jump_conditionally_to_signed_displacement(operand, is_above)
+	end
+	
+	# Jump if the parity flag is not set
+	# (parity is odd)
+	def execute_JNP(operand) # Same as the JPO instruction
+		jump_conditionally_to_signed_displacement(operand, !(@flags.value[PARITY_FLAG].zero?)) # Zero means not set
+	end
+	
+	# Jump if the parity flag is set
+	# (parity is even)
+	def execute_JPE(operand) # Same as the JP instruction
+		jump_conditionally_to_signed_displacement(operand, @flags.value[PARITY_FLAG].zero?) # Non-zero means set
+	end
+	
+	# Jump if the sign flag does not equal the overflow flag
+	# (compare operation: either less than || not greater than or equal)
+	def execute_JL(operand) # Same as the JNGE instruction
+		is_less_than = (@flags.value[SIGN_FLAG] ^ @flags.value[OVERFLOW_FLAG]) == 1
+		jump_conditionally_to_signed_displacement(operand, is_less_than)
+	end
+	
+	# Jump if the sign flag equals the overflow flag
+	# (compare operation: either not less than || greater than or equal)
+	def execute_JGE(operand) # Same as the JNL instruction
+		is_greater_than = @flags.value[SIGN_FLAG] == @flags.value[OVERFLOW_FLAG]
+		jump_conditionally_to_signed_displacement(operand, is_greater_than)
+	end
+	
+	# Jump if the sign flag does not equal the overflow flag OR the zero flag is set
+	# (compare operation: either less than or equal || not greater than)
+	def execute_JLE(operand) # Same as the JNG instruction
+		is_less_than = (@flags.value[SIGN_FLAG] ^ @flags.value[OVERFLOW_FLAG]) == 1
+		is_less_than_or_equal = is_less_than || @flags.value[ZERO_FLAG] == 1
+		jump_conditionally_to_signed_displacement(operand, is_less_than_or_equal)
+	end
+	
+	# Jump if the sign flag equals the overflow flag AND the zero flag is not set
+	# (compare operation: either greater than || not less than or equal)
+	def execute_JG(operand) # Same as the JNLE instruction
+		sf_equals_of = (@flags.value[SIGN_FLAG] ^ @flags.value[OVERFLOW_FLAG]).zero?
+		is_greater_than = sf_equals_of && @flags.value[ZERO_FLAG].zero?
+		jump_conditionally_to_signed_displacement(operand, is_greater_than)
 	end
 	
 	def execute_JCXZ(operand)
@@ -86,6 +186,10 @@ module Executor
 	def execute_RET(operand=nil)
 		@ip.value = pop_stack_word
 	end
+	
+	# -----------------------------------------------------------------
+	# Stack Instructions
+	# -----------------------------------------------------------------
 	
 	def execute_PUSH(operand)
 		push_stack_word operand.value
